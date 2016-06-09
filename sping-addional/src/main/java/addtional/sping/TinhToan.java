c8d614d27a203b17fcb68b1a00fd7ceebe03b6d4
Thanh Lam <tlam26@csc.com>
2016-06-09 15:11:11 +0700

    add
    add

---
+++ sping-addional/src/main/java/addtional/sping/TinhToan.java
@@ -0,0 +1,8 @@
+package addtional.sping;
+
+public class TinhToan {
+	public static void Compute() {
+		System.out.println("This is compute method");
+	}
+
+}
--- spring-project/src/main/java/controller/RosterController.java
+++ spring-project/src/main/java/controller/RosterController.java
@@ -5,16 +5,24 @@ import java.util.List;
 
 import model.Member;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import addtional.sping.TinhToan;
+
 @Controller
 public class RosterController {
+	final static Logger logger = Logger.getLogger(RosterController.class);
 	private List<Member> members = new ArrayList<Member>();
 	public RosterController() {
 		int a = addtional.sping.Math.addnumber(1, 2);
+		TinhToan.Compute();
+		if(logger.isDebugEnabled()){
+		    logger.debug("This is debug");
+		}
 		members.add(new Member("John", "Lennon"));
 		members.add(new Member("Steve", "Jobs"));
 		members.add(new Member("Bill", "Gate"));
